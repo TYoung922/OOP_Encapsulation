858c3637e50f535e53f833b0a2d75518467a0e9b
TercanoNuruva <thyoung89@gmail.com>
2024-11-20 12:44:23 -0700

    Add project files.

---
+++ OOP_Encapsulation/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Encapsulation
+{
+    public class BankAccount
+    {
+        private int balance;
+
+        public BankAccount(int balance)
+        {
+            this.balance = balance;
+        }
+
+        public void Deposit(int amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(int amount) 
+        { 
+            balance -= amount;
+        }
+
+        public int GetBalance() 
+        {
+            return balance;
+        }
+    }
+}
---
+++ OOP_Encapsulation/Program.cs
@@ -0,0 +1,10 @@
+﻿using OOP_Encapsulation;
+
+BankAccount bankAccount = new BankAccount(1000);
+
+bankAccount.Deposit(1000);
+bankAccount.Withdraw(500);
+
+Console.WriteLine(bankAccount.GetBalance());
+
+
